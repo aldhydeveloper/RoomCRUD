a02e200b022ea682536378cba9df2cc9b41d138e
aldhyNS <aldhy.developer@gmail.com>
2021-01-08 10:23:17 +0700

    ROOM CRUD

---
+++ app/src/main/java/com/ns/test1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.ns.test1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupListener()
+
+    }
+
+    fun setupListener() {
+        TODO("Not yet implemented")
+        
+    }
+}
---
+++ app/src/main/java/com/ns/test1/room/Note.kt
@@ -0,0 +1,12 @@
+package com.ns.test1.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val title: String,
+    val note: String
+)
